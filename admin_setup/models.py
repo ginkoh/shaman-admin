d52ac6a6641235733ab496abf04684387b468c44
Gian Bernardo <gianszabernardo@gmail.com>
2020-01-07 02:18:46 -0300

    Create a model for the admin setup

--- admin_setup/models.py
+++ admin_setup/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
+
 
 # Create your models here.
+class AdminSetup(models.Model):
+    app_name = models.CharField(max_length=255)
+    # app_key =
+
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self):
+        return self.app_name
