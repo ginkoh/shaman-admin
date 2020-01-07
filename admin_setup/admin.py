9391d6d16b601205e3a82bf44e3e99dd93711fa7
Gian Bernardo <gianszabernardo@gmail.com>
2020-01-07 02:18:24 -0300

    Register the AdminSetup model on the admin site

--- admin_setup/admin.py
+++ admin_setup/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import AdminSetup
+
 
 # Register your models here.
+class AdminSetupAdmin(admin.ModelAdmin):
+    list_display = ['created', 'updated', 'uuid']
+
+
+admin.site.register(AdminSetup, AdminSetupAdmin)
