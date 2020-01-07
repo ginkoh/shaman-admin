181f4f3e459a7926289f9ef00cf9ed4dd093baff
Gian Bernardo <gianszabernardo@gmail.com>
2020-01-07 02:21:00 -0300

    Modify the chat urls

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('messages/', views.messages, name='messages'),
+    # Get a single conversation object by her uuid.
+    path('conversation/<str:admin_app_uuid>/<str:conversation_uuid>/', views.conversation, name='single_conversation'),
+
+    # Get all conversation objects from an user uuid.
+    path('conversations/<str:admin_app_uuid>/<str:user_uuid>/', views.conversations_from_user, name='conversations_by_user'),
+
+    # path('conversation/', include('rest_framework.urls'), namespace='rest_framework'),
+
     path('<str:room_name>/', views.room, name='room'),
 ]
