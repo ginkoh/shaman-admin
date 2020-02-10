f2347a90565e23a1e2bef1ac0f0bd2aff6a987b2
Gian Bernardo <gianszabernardo@gmail.com>
2020-02-10 01:24:49 -0300

    Add survey models

--- admin_setup/models.py
+++ admin_setup/models.py
@@ -13,3 +13,29 @@ class Entity(models.Model):
 
     def __str__(self):
         return self.app_name
+
+
+class Survey(models.Model):
+    survey_identifier = models.CharField(max_length=255)
+    survey_entity = models.ForeignKey(Entity, on_delete=models.CASCADE, related_name='survey_entity')
+
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self):
+        return self.survey_identifier
+
+
+class SurveyQuestion(models.Model):
+    question_short_title = models.CharField(max_length=255)
+    question_content = models.TextField(max_length=255)
+
+    question_survey = models.ForeignKey(Survey, on_delete=models.CASCADE, related_name='question_survey')
+
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self):
+        return self.question_short_title
